34f88f371b648063dc84eec24a69549822f0e15f
Valencius Apriady <v.apriady@gmail.com>
2024-12-09 15:59:56 +0700

    feat: enabled turbopack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {},
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
