dedec7f78a68353e1ad437fa27098730a01a3676
Valencius Apriady <v.apriady@gmail.com>
2024-12-14 13:54:08 +0700

    refactor: revert unnecessary changes to original

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const { address } = useAppKitAccount();
   return (
     <main className="flex flex-grow flex-col items-center justify-center bg-amber-50">
-      {address} hai
+      {address}
     </main>
   );
 }
--- frontend/app/provider.tsx
+++ frontend/app/provider.tsx
@@ -1,11 +1,41 @@
 "use client";
 
-import { cookieToInitialState, WagmiProvider } from "wagmi";
+import {
+  cookieStorage,
+  cookieToInitialState,
+  createStorage,
+  WagmiProvider,
+} from "wagmi";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { opBNBTestnet } from "@reown/appkit/networks";
 
+import { createAppKit } from "@reown/appkit/react";
+
+import { siweConfig } from "@/libs/siwe";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { wagmiAdapter } from "@/libs/appkit";
 
 const queryClient = new QueryClient();
+
+const wagmiAdapter = new WagmiAdapter({
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  storage: createStorage({ storage: cookieStorage }),
+  networks: [opBNBTestnet],
+  ssr: true,
+});
+
+createAppKit({
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  adapters: [wagmiAdapter],
+  networks: [opBNBTestnet],
+  defaultNetwork: opBNBTestnet,
+  siweConfig: siweConfig,
+  themeMode: "light",
+  features: {
+    analytics: true,
+    onramp: false,
+    swaps: false,
+  },
+});
 
 export default function Provider({
   cookies,
