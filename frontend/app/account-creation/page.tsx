8cc1b4e76f73edae6dd376f520b4b06a013bed08
Imo Madjid <mhmmadjid@gmail.com>
2024-12-15 14:51:38 +0700

    feat:better toast logic

--- frontend/app/account-creation/page.tsx
+++ frontend/app/account-creation/page.tsx
@@ -3,6 +3,9 @@ import NewUserForm from "../components/NewUserForm";
 export default async function AccountCreationPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-50 to-slate-200">
+      <p className="font-cursive absolute left-1/2 top-8 -translate-x-1/2 text-lg font-500 text-slate-400/70">
+        OpenArena
+      </p>
       <NewUserForm />
     </main>
   );
